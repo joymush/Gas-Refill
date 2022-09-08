c5b4d326057a90527275d48aa51f566f8160a9f4
joymush <joy.chemutai@student.moringaschool.com>
2022-09-09 01:25:19 +0300

    Add countdown timer

--- src/index.js
+++ src/index.js
@@ -1,31 +1,27 @@
 // write a function that will load content on the DOM
 const initialize= () => {
     // write a function that fetchs questions from the local server
-    function fetchInformation() {
-
-        let submit = document.getElementsByClassName("form-center")
-        submit.addEventListener("submit", (e)=> {
-            e.preventDefault();
-            const input=document.getElementById('input_section')
-            fetch(`http://localhost:3000/information/${input.value}`)
-            .then((res) => res.json())
-            .then((data) => setInformation(data))
-        })
-
-     }fetchInformation()
-
-     let input_box1;
-     let input_box2;
-     let input_box3;
-     let input_box4;
-
-     input_box1.addEventListener("click",chooseAnswer)
-     input_box2.addEventListener("click",chooseAnswer)
-     input_box3.addEventListener("click",chooseAnswer)
-     input_box4.addEventListener("click",chooseAnswer)
-
-     input_box1=choice1.innerHtml
-     input_box2=choice2.innerHtml
-     input_box3=choice3.innerHtml;
-     input_box4=choice4.innerHtml;     
 }
+    // function for countdown
+    const timeM = document.querySelector('h2');
+    let timeSecond = 5;
+
+    displayTime(timeSecond)
+
+    const countDown = setInterval (()=>{
+        timeSecond--;
+        displayTime(timeSecond);
+        if(timeSecond<= 0 || timeSecond < 1){
+            endTime();
+            clearInterval(countDown);
+        }
+    },1000)
+
+    function displayTime(second){
+        const min = Math.floor(second / 60);
+        const sec = Math.floor(second % 60);
+        timeM.innerHTML = `${min<10 ? '0': ''}${min}:${sec<10 ? '0': ''}${sec}`
+    }
+    function endTime(){
+        timeM.innerHTML = 'DELIVERED'
+    }
